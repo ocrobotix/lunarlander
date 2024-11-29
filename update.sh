64a2ffb743757c4673dad733aa3c83b84eb891c3
ocrobotix <tkim@ocrobotix.com>
2024-11-29 09:30:31 -0800

    Update 1

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# chmod +x <rerun.sh>
-# ./<rerun.sh>
+# chmod +x <update.sh>
+# ./<update.sh>
+mkdocs build --clean    # cleans the site directory by deleting files and clearning cache
 
 git add .
 git commit -m "Update 1"
-# Push the main branch first
-git push origin main
-# Deploy site to gh-pages branch
-mkdocs gh-deploy
+
+git push origin main    # Push the main branch first
+mkdocs gh-deploy        # Deploy site to gh-pages branch
